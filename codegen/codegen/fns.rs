fd9908dd7d5efd0cc68c6f9431322d66a54e261c
Ilya Bylich <ibylich@gmail.com>
2021-11-30 02:47:39 +0300

    add documentation

--- codegen/codegen/fns.rs
+++ codegen/codegen/fns.rs
@@ -4,35 +4,178 @@ use lib_ruby_parser_nodes::{
     Message, MessageField, Node, NodeField,
 };
 
+fn convert_non_code_line_to_yard(line: &str) -> String {
+    let mut output = String::new();
+    let mut in_code_block = false;
+    let mut idx: usize = 0;
+    let chars = line.chars().collect::<Vec<char>>();
+
+    let char_at = |idx: usize| chars.get(idx).map(|c| *c);
+
+    while idx < chars.len() {
+        let c = chars[idx];
+
+        if c == ' ' && char_at(idx + 1) == Some('$') && char_at(idx + 2) == Some('`') {
+            // a very special $` variable
+            idx += 3;
+            output.push_str("&`");
+            continue;
+        }
+
+        if c == '`' {
+            if char_at(idx + 1) == Some('`') {
+                // double back-tick, used to escape inner backtick
+                in_code_block = !in_code_block;
+                idx += 2;
+                output.push('+');
+                continue;
+            }
+            // regular single backtick
+            in_code_block = !in_code_block;
+            idx += 1;
+            output.push('+');
+            continue;
+        } else {
+            // regular char
+            idx += 1;
+            output.push(c);
+            continue;
+        }
+    }
+    assert!(!in_code_block, "not balanced - {:?}", line);
+    output
+}
+
+fn convert_comment_to_yard(comment: &[&str], offset: usize) -> String {
+    let prefix = " ".repeat(offset);
+    let empty_line = format!("{prefix}#\n", prefix = prefix);
+    let mut output = String::new();
+    let mut in_code_block = false;
+    for line in comment {
+        if *line == "```text" {
+            in_code_block = true;
+            if !output.ends_with(&empty_line) {
+                output.push_str(&empty_line);
+            }
+            output.push_str(&format!("{prefix}# @example\n", prefix = prefix));
+            continue;
+        }
+        if *line == "```" {
+            assert!(in_code_block);
+            output.push_str(&empty_line);
+            in_code_block = false;
+            continue;
+        }
+        if in_code_block {
+            output.push_str(&format!(
+                "{prefix}#  {line}\n",
+                prefix = prefix,
+                line = line
+            ));
+            continue;
+        }
+        let line = convert_non_code_line_to_yard(*line);
+        output.push_str(&format!("{prefix}# {line}\n", prefix = prefix, line = line))
+    }
+    assert!(!in_code_block);
+
+    let output = output
+        .lines()
+        .map(|line| {
+            let line = if line.ends_with(" ") {
+                line.strip_suffix(" ").unwrap().to_string()
+            } else {
+                line.to_string()
+            };
+            format!("{}\n", line)
+        })
+        .collect::<Vec<_>>();
+
+    let mut stripped = vec![];
+    let mut prev_line_was_empty = false;
+    for line in output.into_iter() {
+        if line == empty_line {
+            if prev_line_was_empty {
+                continue;
+            } else {
+                stripped.push(line)
+            }
+            prev_line_was_empty = true;
+        } else {
+            stripped.push(line);
+            prev_line_was_empty = false;
+        }
+    }
+
+    stripped.join("")
+}
+
 mod nodes {
-    use super::Node;
+    use super::{convert_comment_to_yard, Node};
 
     pub(crate) fn comment(node: &Node) -> String {
-        node.render_comment("#", 4)
+        convert_comment_to_yard(node.comment, 4)
     }
 }
 
 mod node_fields {
-    use super::NodeField;
+    use super::{convert_comment_to_yard, NodeField};
 
     pub(crate) fn comment(node_field: &NodeField) -> String {
-        node_field.render_comment("#", 6)
+        let comment = convert_comment_to_yard(node_field.comment, 6);
+
+        use lib_ruby_parser_nodes::NodeFieldType::*;
+        let rb_return_type = match node_field.field_type {
+            Node => "Node",
+            Nodes => "::Array<Node>",
+            MaybeNode { .. } => "Node, nil",
+            Loc => "Loc",
+            MaybeLoc => "Loc, nil",
+            Str { .. } => "String",
+            MaybeStr { .. } => "String, nil",
+            StringValue => "String",
+            U8 => "Integer",
+        };
+
+        let return_comment = format!(
+            "      #
+      # @return [{rb_return_type}]
+",
+            rb_return_type = rb_return_type
+        );
+
+        format!("{}{}", comment, return_comment)
     }
 }
 
 mod messages {
-    use super::Message;
+    use super::{convert_comment_to_yard, Message};
 
     pub(crate) fn comment(message: &Message) -> String {
-        message.render_comment("#", 4)
+        convert_comment_to_yard(message.comment, 4)
     }
 }
 
 mod message_fields {
-    use super::MessageField;
+    use super::{convert_comment_to_yard, MessageField};
 
     pub(crate) fn comment(message_field: &MessageField) -> String {
-        message_field.render_comment("#", 6)
+        let comment = convert_comment_to_yard(message_field.comment, 6);
+
+        use lib_ruby_parser_nodes::MessageFieldType::*;
+        let rb_return_type = match message_field.field_type {
+            Str => "String",
+            Byte => "Integer",
+        };
+
+        let return_comment = format!(
+            "      #
+      # @return [{rb_return_type}]
+",
+            rb_return_type = rb_return_type
+        );
+
+        format!("{}{}", comment, return_comment)
     }
 }
 
--- lib/lib-ruby-parser.rb
+++ lib/lib-ruby-parser.rb
@@ -1,24 +1,64 @@
 # frozen_string_literal: true
 
 module LibRubyParser
+  # Representation of any location in the given input
   class Loc
-    attr_accessor :begin, :end
+    # Begin of the `Loc` range
+    #
+    # @return [Integer]
+    attr_accessor :begin
 
+    # End of the `Loc` range
+    #
+    # @return [Integer]
+    attr_accessor :end
+
+    # @param [Hash] options
+    # @option options [Integer] :begin
+    # @option options [Integer] :end
     def initialize(**options)
       @begin = options[:begin] || 0
       @end = options[:end] || 0
     end
   end
 
+  # A token that is emitted by a lexer and consumed by a parser
   class Token
-    attr_accessor :token_type, :token_name, :token_value, :lex_state_before, :lex_state_after, :loc
+    # Numeric representation of the token type, e.g. +132+ (for example) for +tINTEGER+
+    #
+    # @return [Integer]
+    attr_accessor :token_type
 
-    def initialize(
-      token_type:,
-      token_name:,
-      token_value:,
-      loc:
-    )
+    # String type of the token
+    #
+    # @return [String]
+    attr_accessor :token_name
+
+    # Value of the token, e.g +“42”+ for +42+
+    #
+    # @return [String]
+    attr_accessor :token_value
+
+    # Lex state before token was read
+    #
+    # @return [Integer]
+    attr_accessor :lex_state_before
+
+    # Lex state after token was read
+    #
+    # @return [Integer]
+    attr_accessor :lex_state_after
+
+    # Location of the token
+    #
+    # @return [Loc]
+    attr_accessor :loc
+
+    # @param token_type [Integer]
+    # @param token_name [String]
+    # @param token_value [String]
+    # @param loc [Loc]
+    def initialize(token_type:, token_name:, token_value:, loc:)
       @token_type = token_type
       @token_name = token_name
       @token_value = token_value
@@ -28,9 +68,26 @@ module LibRubyParser
     end
   end
 
+  # Diagnostic message that comes from the parser when there’s an error or warning
   class Diagnostic
-    attr_accessor :level, :message, :loc
+    # Level of the diagnostic (+:error+ or +:warning+)
+    #
+    # @return [Symbol] +:error+ or +:warning+
+    attr_accessor :level
 
+    # Message of the diagnostic
+    #
+    # @return [DiagnosticMessage]
+    attr_accessor :message
+
+    # Location of the diagnostic
+    #
+    # @return [Loc]
+    attr_accessor :loc
+
+    # @param level [Symbol]
+    # @param message [DiagnosticMessage]
+    # @param loc [Loc]
     def initialize(level:, message:, loc:)
       @level = level
       @message = message
@@ -38,18 +95,59 @@ module LibRubyParser
     end
   end
 
+  # A class that represents a comment in Ruby
   class Comment
-    attr_accessor :location, :kind
+    # Location of the comment (starts with # and ends with the last char)
+    #
+    # @return [Loc]
+    attr_accessor :location
 
+    # Kind of the comment (+:inline+ or +:document+ or +:unknown+)
+    #
+    # @return [Symbol] +:inline+ or +:document+ or +:unknown+
+    attr_accessor :kind
+
+    # @param location [Loc]
+    # @param location [kind]
     def initialize(location:, kind:)
       @location = location
       @kind = kind
     end
   end
 
+  # Representation of a magic comment in Ruby
   class MagicComment
-    attr_accessor :kind, :key_l, :value_l
+    # Kind of a magic comment, can be
+    #
+    # 1. +encoding+
+    # 2. +frozen_string_literal+
+    # 3. +warn_indent+
+    # 4. +shareable_constant_value+
+    #
+    # @return [Symbol]
+    attr_accessor :kind
 
+    # Location of the “key”
+    #
+    # @example
+    #   # encoding: utf-8
+    #     ~~~~~~~~
+    #
+    # @return [Loc]
+    attr_accessor :key_l
+
+    # Location of the “value”
+    #
+    # @example
+    #   # encoding: utf-8
+    #               ~~~~~
+    #
+    # @return [Loc]
+    attr_accessor :value_l
+
+    # @param kind [Symbol]
+    # @param key_l [Loc]
+    # @param value_l [Loc]
     def initialize(kind:, key_l:, value_l:)
       @kind = kind
       @key_l = key_l
