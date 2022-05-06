a7a874fd6b92bf0211a9442749ec80ce9aa020af
Ilya Bylich <ibylich@gmail.com>
2022-05-07 00:19:14 +0200

    bump c library

--- c-bindings/build.mk
+++ c-bindings/build.mk
@@ -1,7 +1,6 @@
-VERSION = v3.0.0
+VERSION = v4.0.2+ruby-3.1.1
 
 C_BINDINGS_BASE_URL = https://github.com/lib-ruby-parser/c-bindings/releases/download/$(VERSION)
-HEADER_URL = $(C_BINDINGS_BASE_URL)/lib-ruby-parser.h
 
 c-bindings/libruby_parser_c-$(TARGET).$(A):
 	$(call download_file, $(C_BINDINGS_BASE_URL)/libruby_parser_c-$(TARGET).$(A), $@)
@@ -10,5 +9,6 @@ c-bindings/lib-ruby-parser.h:
 	$(call download_file, $(C_BINDINGS_BASE_URL)/lib-ruby-parser.h, $@)
 
 c-bindings/clean:
-	rm -f libruby_parser_c-$(TARGET).$(A)
+	rm -f c-bindings/*.a
+	rm -f c-bindings/*.lib
 	rm -f c-bindings/lib-ruby-parser.h
