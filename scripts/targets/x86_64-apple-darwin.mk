5ce536420b2d610dfc778cc207266c0ea06f38a7
Ilya Bylich <ibylich@gmail.com>
2022-05-07 00:19:58 +0200

    use new codegen execcutable to generate {messages,nodes}.{h,rb}

--- scripts/targets/x86_64-apple-darwin.mk
+++ scripts/targets/x86_64-apple-darwin.mk
@@ -3,6 +3,7 @@ $(info Compiling x86_64-apple-darwin target)
 O = o
 A = a
 DYLIB = bundle
+EXE =
 
 BENCHMARK_RUNNER_ASSET_NAME = rust-parser-x86_64-apple-darwin
 
--- scripts/targets/x86_64-pc-windows-gnu.mk
+++ scripts/targets/x86_64-pc-windows-gnu.mk
@@ -3,6 +3,7 @@ $(info Compiling x86_64-pc-windows-gnu target)
 O = o
 A = a
 DYLIB = so
+EXE = .exe
 
 BENCHMARK_RUNNER_ASSET_NAME = rust-parser-x86_64-pc-windows-gnu.exe
 
